ae4828a27d1bf11a6b5101a409e771fedb6673b1
Faisal Iqbal <87160666+iqbalf203@users.noreply.github.com>
2023-08-09 17:23:35 +0530

    Add files via upload
    Added patientId, doctorId and getByPatientId

---
+++ appointmentservice1/src/main/java/com/hospital/app/controller/AppointmentController.java
@@ -0,0 +1,59 @@
+package com.hospital.app.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hospital.app.exception.InvalidTimeException;
+import com.hospital.app.exception.NotFoundException;
+import com.hospital.app.model.Appointment;
+import com.hospital.app.service.AppointmentService;
+
+@RestController
+@RequestMapping("/appointments")
+public class AppointmentController {
+
+  @Autowired
+  private AppointmentService appointmentService;
+  
+  @GetMapping
+  public ResponseEntity<List<Appointment>> getAllAppointments() {
+    return ResponseEntity.ok(appointmentService.getAllAppointments());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<List<Appointment>> getAppointmentById(@PathVariable int id) {
+    return ResponseEntity.ok(appointmentService.getAppointmentByPatientId(id));
+  }
+
+  @PostMapping
+  public ResponseEntity<Appointment> saveAppointment(@RequestBody Appointment appointment) {
+    return ResponseEntity.ok(appointmentService.saveAppointment(appointment));
+  }
+
+  @PatchMapping("/{id}")
+  public ResponseEntity<Appointment> updateAppointment(@RequestBody Appointment appointment) throws NotFoundException, InvalidTimeException {
+    appointmentService.updateAppointment(appointment);
+    return ResponseEntity.ok(appointment);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> deleteAppointmentById(@PathVariable Long id) {
+    appointmentService.deleteAppointmentById(id);
+    return ResponseEntity.ok("");
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<?> handleException(NotFoundException e) {
+    return ResponseEntity.badRequest().body(e.getMessage());
+  }
+}
